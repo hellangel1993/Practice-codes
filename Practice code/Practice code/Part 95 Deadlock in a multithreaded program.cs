c63540c8fbf09ceae0547dc2baf2d06406da648b
Himanshu Chowdhary <himanshu.chowdhary@qvantel.com>
2019-06-07 19:54:13 +0530

    video source https://www.youtube.com/watch?v=px4bXKYb2-Y&list=PLAC325451207E3105&index=96

--- Practice code/Practice code/Part 95 Deadlock in a multithreaded program.cs
+++ Practice code/Practice code/Part 95 Deadlock in a multithreaded program.cs
@@ -34,77 +34,77 @@ namespace Practice_code
             Console.WriteLine("Main Completed");
         }
     }
+    //uncomment this line to run this code.
+    //public class Account
+    //{
+    //    double _balance;
+    //    int _id;
 
-    public class Account
-    {
-        double _balance;
-        int _id;
+    //    public Account(int id, double balance)
+    //    {
+    //        this._id = id;
+    //        this._balance = balance;
+    //    }
 
-        public Account(int id, double balance)
-        {
-            this._id = id;
-            this._balance = balance;
-        }
+    //    public int ID
+    //    {
+    //        get
+    //        {
+    //            return _id;
+    //        }
+    //    }
 
-        public int ID
-        {
-            get
-            {
-                return _id;
-            }
-        }
+    //    public void Withdraw(double amount)
+    //    {
+    //        _balance -= amount;
+    //    }
 
-        public void Withdraw(double amount)
-        {
-            _balance -= amount;
-        }
+    //    public void Deposit(double amount)
+    //    {
+    //        _balance += amount;
+    //    }
+    //}
 
-        public void Deposit(double amount)
-        {
-            _balance += amount;
-        }
-    }
+    //public class AccountManager
+    //{
+    //    Account _fromAccount;
+    //    Account _toAccount;
+    //    double _amountToTransfer;
 
-    public class AccountManager
-    {
-        Account _fromAccount;
-        Account _toAccount;
-        double _amountToTransfer;
+    //    public AccountManager(Account fromAccount,
+    //        Account toAccount, double amountToTransfer)
+    //    {
+    //        this._fromAccount = fromAccount;
+    //        this._toAccount = toAccount;
+    //        this._amountToTransfer = amountToTransfer;
+    //    }
 
-        public AccountManager(Account fromAccount,
-            Account toAccount, double amountToTransfer)
-        {
-            this._fromAccount = fromAccount;
-            this._toAccount = toAccount;
-            this._amountToTransfer = amountToTransfer;
-        }
+    //    public void Transfer()
+    //    {
+    //        Console.WriteLine(Thread.CurrentThread.Name
+    //            + " trying to acquire lock on "
+    //            + _fromAccount.ID.ToString());
+    //        lock (_fromAccount)
+    //        {
+    //            Console.WriteLine(Thread.CurrentThread.Name
+    //                + " acquired lock on "
+    //                + _fromAccount.ID.ToString());
 
-        public void Transfer()
-        {
-            Console.WriteLine(Thread.CurrentThread.Name
-                + " trying to acquire lock on "
-                + _fromAccount.ID.ToString());
-            lock (_fromAccount)
-            {
-                Console.WriteLine(Thread.CurrentThread.Name
-                    + " acquired lock on "
-                    + _fromAccount.ID.ToString());
+    //            Console.WriteLine(Thread.CurrentThread.Name
+    //                + " suspended for 1 second");
 
-                Console.WriteLine(Thread.CurrentThread.Name
-                    + " suspended for 1 second");
+    //            Thread.Sleep(1000);
 
-                Thread.Sleep(1000);
+    //            Console.WriteLine(Thread.CurrentThread.Name +
+    //                " back in action and trying to acquire lock on "
+    //                + _toAccount.ID.ToString());
 
-                Console.WriteLine(Thread.CurrentThread.Name +
-                    " back in action and trying to acquire lock on "
-                    + _toAccount.ID.ToString());
-
-                lock (_toAccount)
-                {
-                    _fromAccount.Withdraw(_amountToTransfer);
-                    _toAccount.Deposit(_amountToTransfer);
-                }
-            }
-        }
-    }
+    //            lock (_toAccount)
+    //            {
+    //                _fromAccount.Withdraw(_amountToTransfer);
+    //                _toAccount.Deposit(_amountToTransfer);
+    //            }
+    //        }
+    //    }
+    //}
 }
