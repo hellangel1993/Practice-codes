ad0494c4a5bf732e19c2616867321922c8f70a46
Himanshu Chowdhary <himanshu.chowdhary@qvantel.com>
2019-06-07 20:16:18 +0530

    video source https://www.youtube.com/watch?v=aDCAMwTgiZU&list=PLAC325451207E3105&index=97

---
+++ Practice code/Practice code/Part 97 Performance of a multithreaded program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Practice_code
+{
+    class Part_97_Performance_of_a_multithreaded_program
+    {
+        public static void EvenNumberSum()
+        {
+            double sum = 0;
+            for (int i = 0; i < 500000000; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Sum of Even Number= {0}", sum);
+        }
+
+        public static void OddNumbersSum()
+        {
+            double sum = 0;
+            for (int i = 0; i < 500000000; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Sum of the Odd = {0}",sum);
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            EvenNumberSum();
+            OddNumbersSum();
+            stopwatch.Stop();
+            Console.WriteLine("Total time: "+stopwatch.ElapsedTicks);
+
+            Stopwatch stopwatch1 = Stopwatch.StartNew();
+            Thread T1 = new Thread(EvenNumberSum);
+            Thread T2 = new Thread(OddNumbersSum);
+            T1.Start();
+            T2.Start();
+            T1.Join();
+            T2.Join();
+            stopwatch1.Stop();
+            Console.WriteLine("Total time: " + stopwatch1.ElapsedTicks);
+        }
+    }
+}
