6f0dc559b35da5c942aec0c50ec7a8cb85a243ee
Himanshu Chowdhary <himanshu.chowdhary@qvantel.com>
2019-06-07 19:00:21 +0530

    source video https://www.youtube.com/watch?v=pjzplzR8E3U&list=PLAC325451207E3105&index=93

---
+++ Practice code/Practice code/Part 93 Protecting shared resources from concurrent access in multithreading.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Practice_code
+{
+    class Part_93_Protecting_shared_resources_from_concurrent_access_in_multithreading
+    {
+        static int Total = 0;
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Thread T1 = new Thread(Part_93_Protecting_shared_resources_from_concurrent_access_in_multithreading.AddOneMillion);
+            Thread T2 = new Thread(Part_93_Protecting_shared_resources_from_concurrent_access_in_multithreading.AddOneMillion);
+            Thread T3 = new Thread(Part_93_Protecting_shared_resources_from_concurrent_access_in_multithreading.AddOneMillion);
+
+            T1.Start();
+            T2.Start();
+            T3.Start();
+
+            T1.Join();
+            T2.Join();
+            T3.Join();
+
+            Console.WriteLine("Total= "+Total);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedTicks);
+        }
+
+        static object _lock = new object();
+        public static void AddOneMillion()
+        {
+            for (int i = 1; i <=1000000; i++)
+            {
+                //uncomment to see the which function is fast.
+                Interlocked.Increment(ref Total);//this is the inbuild function to increment the variable without crossthreading problem
+                //lock(_lock)
+                //{
+                //    Total++;
+                //}
+            }
+        }
+    }
+}
